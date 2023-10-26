4188a611250563415e89ebbbf99f487611943fe6
DanilovA4 <DanilovA4@RUDSFC040717>
2023-10-26 09:38:34 +0300

    Add project files.

---
+++ ConsoleApp1/Models/Person.cs
@@ -0,0 +1,39 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set {
+                    if (value == _firstName) return;
+                    _firstName = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _fullName;
+        public string FullName 
+        { 
+            get => _fullName;
+            set { _fullName = FirstName; }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person();
+
+            tom.FirstName = "Tom";
+            Console.WriteLine(tom.FirstName);
+            Console.WriteLine(tom.FullName);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
